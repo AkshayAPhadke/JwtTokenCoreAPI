0184087bdadfc2eebc253cc5f56e49850b99e81e
AkshayPhadke <akshayaphadke@gmail.com>
2024-05-03 08:06:32 +0530

    changes

---
+++ JwtTokenCoreAPI/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtTokenCoreAPI.Controllers
+{
+
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Missing 3 Students Today out of 12");
+        }
+    }
+}
